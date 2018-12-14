cf2add6e21e21a7cfaef2c651c400c195ec918ad
Lucas <joselucasrs@gmail.com>
2018-12-14 16:12:41 -0200

    login do responsavel consertado v2

--- omega/resources/views/pages/index.blade.php
+++ omega/resources/views/pages/index.blade.php
@@ -4,15 +4,15 @@
 <div class="container">
     <div class="col-md-6 col-sm-10 mx-auto formCard">
         <h1 class="col-12">Login</h1>
-        <form method="POST" action="{{ route('login-r') }}">
+        <form method="POST" action="{{ route('login-responsavel') }}">
             {{ csrf_field() }}
-            <div class="form-group{{ $errors->has('email_empresa') ? ' has-error' : '' }}">
+            <div class="form-group{{ $errors->has('email_responsavel') ? ' has-error' : '' }}">
                 <label>Email</label>
-                <input id="email" type="email" class="form-control" name="email_empresa" value="{{ old('email_empresa') }}" required autofocus placeholder="Digite o email">
+                <input id="email" type="email" class="form-control" name="email_responsavel" value="{{ old('email_responsavel') }}" required autofocus placeholder="Digite o email">
 
-                @if ($errors->has('email_empresa'))
+                @if ($errors->has('email_responsavel'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('email_empresa') }}</strong>
+                        <strong>{{ $errors->first('email_responsavel') }}</strong>
                     </span>
                 @endif
             </div>
