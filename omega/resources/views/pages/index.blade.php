98426a535c7428fbb3625378821816ddd4dbcf6b
Lucas <joselucasrs@gmail.com>
2018-12-21 18:02:08 -0200

    bordas vermelhas novas e link no card

--- omega/resources/views/pages/index.blade.php
+++ omega/resources/views/pages/index.blade.php
@@ -1,37 +1,35 @@
 @extends ('layouts.app')
 @section('content')
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top " style="background-color: #1f648b;">
 
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top " style="background-color: #1f648b;">
-        <div class="container">
-            <a class="navbar-brand" href="">Projeto Omega</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="\login-empresa"> Entrar <i class="fas fa-user"></i>
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+        <a class="navbar-brand" href="">Projeto Omega</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="\login-empresa"> Entrar <i class="fas fa-user"></i>
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+            </ul>
         </div>
-    </nav>
-    <div class="container-fluid">
-        <div class="col-md-6 col-md-6 " style="margin-top: 60px;">
-            <div class="card" style="width: 18rem;">
+
+</nav>
+<div class="container">
+        <div class="col-md-6" style="margin-top: 250px; margin-left: 450px;">
+            <div class="card" style="width: 25rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <h3 class="card-title">Projeto Omega</h3>
+                    <h6 class="card-subtitle mb-2 text-muted">Facilidade de Monitoramento</h6>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <a href="#" class="btn-outline-danger">Card link</a>
+
                 </div>
             </div>
         </div>
-    </div>
-
+</div>
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
