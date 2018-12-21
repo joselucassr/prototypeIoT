8ca9e7c5531c30723067ca686daa5527fe1adc20
Lucas <joselucasrs@gmail.com>
2018-12-21 15:00:22 -0200

    bordas vermelhas novas e link no card

---
+++ omega/resources/views/login/loginInicio.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.app')
+@section('content')
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand">Navbar</a>
+        <form class="form-inline">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Entrar<i class="far fa-user-circle"></i></button>
+        </form>
+    </nav>
+@endsection
